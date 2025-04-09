edeeb07f3f406b13a092c5efeba11a8d564e94be
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2025-04-09 17:36:48 +0200

    Added block styles and chnage files around

--- functions.php
+++ functions.php
@@ -1,42 +1,13 @@
 <?php
 
-function my_custom_block_styles() {
-    wp_enqueue_style(
-        'my-image-block-style',
-        get_template_directory_uri() . '/my-image-block-style.css',
-        array( 'wp-edit-blocks' ),
-        '1.0'
-    );
-   
+function my_style_red(){
 
     register_block_style(
         'core/image',
         array(
-            'name'         => 'purple-border',
-            'label'        => __( 'Purple Border, slightly rounded', 'my-theme' ),
-            'style_handle' => 'my-image-block-style'
+            'name'         => 'red-border',
+            'label'        => __( 'Red Border', 'my-theme' )
         )
     );
-
-    register_block_style(
-        array( 'core/image' ),
-        array(
-            'name'         => 'red-border',
-            'label'        => __( 'Red Border', 'plain-pauli' ),
-            'style_data'=> array(
-                            'border' => array(
-                            'color' => '#cf2e2e',
-                             'style' => 'solid',
-                             'width' => '4px',
-                             'radius' => '15px'
-                            ),
-			            'shadow' => array(
-                            'var(--wp--preset--shadow--sharp)'
-                        )
-                        )
-
-                    )
-                        );
-};
-
-add_action( 'init', 'my_custom_block_styles' );
+}
+add_action( 'init', 'my_style_red' );
