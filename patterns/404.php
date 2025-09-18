31ff79bfe45ba5c77e751187821787aed9339e40
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2025-09-18 20:36:18 +0200

    Changes from WordPress Playground - add colors - change letter spacing - add 404 template
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2Fbph%2Fplain-pauli%2Fplayground-changes-2025-09-18T18-36-13-985Z%2F.%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2Fbph%2Fplain-pauli%2Fmain%2F.%2Fplayground.zip)

---
+++ patterns/404.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: 404
+ * Slug: plain-pauli/404
+ * Inserter: no
+ */
+?>
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Oops that didn\'t work. ', 'plain-pauli');?></p>
+<!-- /wp:paragraph -->
